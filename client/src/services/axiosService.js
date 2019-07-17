696c8c0a656a07274bcc5c5ed50fcc87cfd5f646
Emmanuel Oliveira <emmanueloliveira@iusnatura.com.br>
2019-07-17 08:57:39 -0300

    Client configs

--- client/src/services/axiosService.js
+++ client/src/services/axiosService.js
@@ -1,4 +1,4 @@
 import Axios from 'axios';
 
-const Service = Axios.create({ baseURL: `https://massas-api.herokuapp.com`});
+const Service = Axios.create({ baseURL: `http://localhost:${process.env.PORT || 3001}`});
 export default Service;
